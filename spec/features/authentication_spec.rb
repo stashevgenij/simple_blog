8ddd502e4b4d74918a1d9856c2ac61260dd76da2
Chris Kilian <chriskilian5@yandex.ru>
2020-11-09 04:19:02 +0700

    Add authentication spec
    Added 3 test features of authentication

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'Signing up', js: false do
+  let(:user) { build :user }
+
+  scenario 'register user' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    expect { click_on 'Sign up' }.to change(User, :count).by(1)
+  end
+end
+
+feature 'Signing in', js: false do
+  let(:user) { create :user }
+
+  scenario 'authenticates user with correct data' do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    expect(page).to have_css('a', text: 'Logout')
+  end
+end
+
+feature 'Signing out', js: false do
+  let(:user) { create :user }
+
+  scenario 'log out user' do
+    sign_in user
+    click_on 'Logout'
+    expect(page).to have_css('a', text: 'Login')
+  end
+end
